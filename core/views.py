e24ffc34de06f9ba31ee1c4769f9afc047175ac1
Elshaddai Mhango <emhango8486@kvcc.edu>
2026-06-06 15:07:45 -0400

    Temporarily email

--- core/views.py
+++ core/views.py
@@ -51,12 +51,12 @@ MESSAGE
                 to=["elshaddaimhango@gmail.com"],
                 reply_to=[submission.email],
             )
-
-#            try:
-#                email.send()
-#           except Exception as e:
-#                print("EMAIL ERROR:", e)
-#
+            try:
+                email.send()
+                print("EMAIL SENT")
+            except Exception as e:
+                print("EMAIL ERROR:", str(e))
+                
             return redirect("contact_success")
 
     else:
